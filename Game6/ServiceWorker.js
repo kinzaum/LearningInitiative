29441b6c1fb98024aac74ee66d9dc0bc1cbbefa6
fabioensign <fabio.dias@usa.com>
2026-03-17 00:01:01 -0600

    Adding new Game6

---
+++ Game6/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-VoicePainter-1.0";
+const contentToCache = [
+    "Build/Game06.loader.js",
+    "Build/Game06.framework.js",
+    "Build/Game06.data",
+    "Build/Game06.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
